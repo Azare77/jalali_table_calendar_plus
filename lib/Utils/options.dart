b9277a0d386cf9624769061780484f4a6d010a0d
Marjan DavoodiNejad <marjan.dn96@yahoo.com>
2024-10-12 11:21:21 +0330

    Add calenar and day item size properties and a certain style for today item -Remove unnecessary space on top of gridview

--- lib/Utils/options.dart
+++ lib/Utils/options.dart
@@ -4,21 +4,26 @@ class JalaliTableCalendarOption {
   final TextStyle? daysOfWeekStyle;
   final TextStyle? headerStyle;
   final TextStyle? daysStyle;
+  final TextStyle? todayStyle;
   final Color? currentDayColor;
   final Color? selectedDayShapeColor;
   final Color? selectedDayColor;
   final List<String>? daysOfWeekTitles;
   final EdgeInsets? headerPadding;
+  final double calendarHeight;
+  final double dayItemSize;
 
   JalaliTableCalendarOption({
     this.daysOfWeekStyle,
     this.headerStyle,
     this.daysStyle,
+    this.todayStyle,
     this.currentDayColor,
     this.selectedDayColor,
     this.selectedDayShapeColor,
     this.daysOfWeekTitles,
+    this.calendarHeight = 300,
+    this.dayItemSize = 50,
     this.headerPadding,
-  }) : assert(daysOfWeekTitles == null || daysOfWeekTitles.length == 7,
-            "daysOfWeekTitles length must be 7");
+  }) : assert(daysOfWeekTitles == null || daysOfWeekTitles.length == 7, "daysOfWeekTitles length must be 7");
 }
